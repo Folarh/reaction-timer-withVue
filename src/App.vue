321cd9486d6cabbbbcce50815efd5c0d5639b6a5
unknown <folarintaiye@gmail.com>
2022-12-16 03:57:39 +0100

    first  vue project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!-- <NewForm/> -->
+<div>
+<h1>{{title}}</h1>
+<p>{{subtitle}}</p>
+<button @click="start" :disabled="isPlaying">Play</button>
+<BlockCut v-if="isPlaying" :delay="delay" @end="endGame"/>
+<ShowResults v-if="showResults" :score="score"/>
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BlockCut from "./components/BlockCut.vue"
+import ShowResults from "./components/ShowResults.vue"
+// import NewForm from "./components/NewForm.vue"
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { BlockCut, ShowResults},
+  data(){
+    return{
+      title: "Reaction Timer",
+      subtitle: "click on the box to see how fast you can react",
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    
+    }
+   
+  },
+   methods: {
+      start(){
+        this.delay = 2000 + Math.random()* 5000
+        this.isPlaying= true;
+         this.showResults= false
+       
+      },
+      endGame(reactionTimer){
+        this.score= reactionTimer
+       this.isPlaying= false
+         this.showResults= true
+      }
+    }
+ 
   }
-}
 </script>
 
 <style>
@@ -22,5 +54,30 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+div{
+  
+  margin: 0 auto;
+  width: 50%;
+  background:whitesmoke;
+  color: grey;
+}
+
+button{
+  background: crimson;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin:10px;
+}
+
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/ShowResults.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction Time: {{score}}ms</p>
+  <p class="rank">{{rank}}</p>
+</template>
+
+<script>
+export default {
+props: ["score"],
+data(){
+    return{
+        rank: null
+    }
+},
+mounted(){
+    if(this.score <250){
+        this.rank= "King"
+    }else if(this.score <400){
+        this.rank= "monster"
+    }else{
+        this.rank= "snail...hahahahaa"
+    }
+}
+}
+</script>
+
+<style>
+.rank{
+    font-size: 1.4em;
+    color: crimson;
+    font-weight: bold;
+}
+</style>
